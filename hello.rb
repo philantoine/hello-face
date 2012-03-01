77cf1e6b87bc28e656aecd09db955935d7adeccc
unknown <Stephanie@patawpha.Retz>
2012-03-01 16:41:13 +0100

    version avec les images

--- hello.rb
+++ hello.rb
@@ -1,16 +1,33 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'koala'
 
-oauth_access_token = ENV['OAUTH_ACCESS_TOKEN']
+# TODO: nous assurer que nous vérifions bien l'identité du serveur
+require 'openssl'
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
+oauth_access_token = ENV['OAUTH_ACCESS_TOKEN'] || ""
 graph = Koala::Facebook::API.new(oauth_access_token)
 
 get '/' do
-  content_type 'application/json', :charset => 'utf-8'
-  graph.get_object('me')
-end
+  friends = graph.get_connections('me', 'friends')[0..49]
+  pictures = graph.batch do |batch_api|
+    friends.each do |friend|
+      batch_api.get_picture(friend['id'])
+    end
+  end
+  
+  @friends = []
 
-get '/about' do
-  "Wooahh!"
+  friends.each_with_index do |f, index|
+    friend = OpenStruct.new
+    friend.name = f['name']
+    friend.facebook_id = f['id']
+    friend.picture_url = pictures[index]
+    @friends << friend
+  end
+
+ erb :index
 end
 
 =begin
@@ -18,10 +35,6 @@ require "bundler/setup"
 require 'koala'
 require 'ap'
 
-# TODO: nous assurer que nous vérifions bien l'identité du serveur
-
-require 'openssl'
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 oauth_access_token = ""
 graph = Koala::Facebook::API.new(oauth_access_token)
---
+++ readme.md
@@ -0,0 +1,13 @@
+### How to create a facebook token
+
+http://developers.facebook.com/tools/explorer/
+
+bundle exec heroku config:add OAUTH_ACCESS_TOKEN=AAACEdEose0cBAEIZCkDuvslxMGg56NP7wlLL8n7sUtXXNzuWQbDUAUhS4MPOxA7KTJrZCNDv0HMiiHZArrtsAVaDjZCXNJIwC5cjDRa2aDBQ6VRdkEcR
+
+### How to deploy
+
+open git bash prompt
+
+### How to run locally
+
+ruby hello.rb
